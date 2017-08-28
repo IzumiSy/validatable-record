768d716fee92d700dd036568a362799b2814dad2
IzumiSy <beetle-noise@gmx.com>
2017-08-28 21:47:40 +0900

    Fix

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const I = require("immutable");
 const validate = require("validate.js");
 
-export.default = function ValidatableRecord(args, rules = {}) {
+module.exports = function ValidatableRecord(args, rules) {
   const _prototype = I.Record.prototype;
+  const _rules = rules || {}
 
   _prototype._validationErrors = []
   _prototype.validate = function() {
     _prototype._validationErrors =
-      (validate(this.toJS(), rules, { format: "flat" }) || []);
+      (validate(this.toJS(), _rules, { format: "flat" }) || []);
     return !!!_prototype._validationErrors.length
   }
   _prototype.getErrors = function() {
