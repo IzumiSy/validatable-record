512f082c30493f52c111ddf30ac63f3811c53a3c
IzumiSy <beetle-noise@gmx.com>
2017-08-28 20:24:24 +0900

    Implement

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TODO
+const I = require("immutable");
+const validate = require("validate.js");
+
+export.default = function ValidatableRecord(args, rules = {}) {
+  const _prototype = I.Record.prototype;
+
+  _prototype._validationErrors = []
+  _prototype.validate = function() {
+    _prototype._validationErrors =
+      (validate(this.toJS(), rules, { format: "flat" }) || []);
+    return !!!_prototype._validationErrors.length
+  }
+  _prototype.getErrors = function() {
+   	return _prototype._validationErrors;
+  }
+
+  return I.Record(args);
+}
